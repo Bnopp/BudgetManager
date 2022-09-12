985b51648edf29fdff095917e6a7d5b7efd64c29
Serghei Diulgherov <pl03jlu@eduvaud.ch>
2022-09-12 16:30:24 +0200

    BudgetManager v0.1.0
    - Added classes for : categories, payments
    - Started User Interface

---
+++ BudgetManager/BudgetManager/Category.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager
+{
+    public class Category
+    {
+        private string _name;
+        private string _description;
+        private double _limit;
+        private List<Payement> _payments;
+
+        public string Name { get { return _name; } }
+        public string Description { get { return _description; } }
+        public double Limit { get { return _limit; } set { _limit = value; } }
+        public List<Payement> Payments { get { return _payments; } set { _payments = value; } }
+
+        public Category(string name, string description, double limit, List<Payement> payments)
+        {
+            _name = name;
+            _description = description;
+            _limit = limit;
+            _payments = payments;
+        }
+    }
+}
---
+++ BudgetManager/BudgetManager/Payement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager
+{
+    public class Payement
+    {
+        private string _title;
+        private double _amount;
+        private Category _category;
+        private DateTime _date;
+        
+        public string Title { get { return _title; } }
+        public double Amount { get { return _amount; } }
+        private Category Category { get { return _category; } }
+        public DateTime Date { get { return _date; } }
+
+        public Payement(string title, double amount, Category category, DateTime date)
+        {
+            _title = title;
+            _amount = amount;
+            _category = category;
+            _date = date;
+        }
+    }
+}
