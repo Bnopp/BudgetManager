19167dc4954679284e09eb18c935e567841b6dd1
Serghei Diulgherov <pl03jlu@eduvaud.ch>
2022-09-26 16:28:07 +0200

    BudgetManager v0.1.5
    - Started Creating User Interface

--- BudgetManager/BudgetManager/MainWindow.xaml.cs
+++ BudgetManager/BudgetManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BudgetManager.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace BudgetManager
         public MainWindow()
         {
             InitializeComponent();
+
+            MainFrame.Navigate(new HomePage());
         }
     }
 }
